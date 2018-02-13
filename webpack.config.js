67cd050e4f88b42962adfe9c564070c4004b041a
ilya <directblow@icloud.com>
2018-02-13 14:33:39 +0300

    add webpack encore

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,17 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    //.addEntry('bootstrap', './assets/css/global.scss')
-    //.addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('logo', './assets/images/logo.png')
+    //addStyleEntry('styles', './assets/app.scss')
 
     // uncomment if you use Sass/SCSS files
     //.enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
