32b6d02c9eaeb31fb9f0d7d2e57c5c28997144e9
ilya <directblow@icloud.com>
2018-02-21 16:41:31 +0300

    add remember me button

---
+++ src/Form/AnswerType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Answer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnswerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text', TextType::class,array('label' => ' '))
+            ->add('isTrue', RadioType::class,array('label'=>' we','required'=>false))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // uncomment if you want to bind to a class
+            'data_class' => Answer::class,
+        ]);
+    }
+}
